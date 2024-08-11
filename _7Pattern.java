f33aea6169cf4a3680ac7e6387351504ca992ff3
yadavpriya12 <169046727+yadavpriya12@users.noreply.github.com>
2024-08-11 23:06:04 +0530

    Add files via upload

---
+++ _7Pattern.java
@@ -0,0 +1,13 @@
+public class _7Pattern {
+    
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=i;j>0;j--)
+            {
+                System.out.print(" "+j);
+            }
+            System.out.println(" ");
+        }
+    }
+}
