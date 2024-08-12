d0730fcea86e3eebb2069bc6491ee06d430550b0
yadavpriya12 <169046727+yadavpriya12@users.noreply.github.com>
2024-08-12 13:40:42 +0530

    Add files via upload

---
+++ _8Pattern.java
@@ -0,0 +1,12 @@
+public class _8Pattern {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=5;j>0;j--)
+            {
+                System.out.print(" "+j);
+            }
+            System.out.println(" ");
+        }
+    }
+}
