9ba909757bb1d807fb7e04ed8a84ac435a60a0c5
yadavpriya12 <169046727+yadavpriya12@users.noreply.github.com>
2024-08-13 15:14:23 +0530

    Add files via upload

---
+++ _10Pattern.java
@@ -0,0 +1,12 @@
+public class _10Pattern {
+    public static void main(String[] args) {
+        for(int i=5;i>=1;i--)
+        {
+            for(int j=i;j<=5;j++)
+            {
+                System.out.print(" "+j);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ _11Pattern.java
@@ -0,0 +1,12 @@
+public class _11Pattern {
+    public static void main(String[] args) {
+        for(int i=0;i<=5;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(" "+j);
+            }
+            System.out.println(" "+6);
+        }
+    }
+}
