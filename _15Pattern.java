d70afffa29755d2e43d6896a94cec284538b0f19
yadavpriya12 <169046727+yadavpriya12@users.noreply.github.com>
2024-08-16 21:50:44 +0530

    Add files via upload

---
+++ _15Pattern.java
@@ -0,0 +1,13 @@
+public class _15Pattern {
+    public static void main(String[] args) {
+        
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=i;j<=5;j++)
+            {
+                System.out.print(" *");
+            }
+            System.out.println(" ");
+        }
+    }
+}
