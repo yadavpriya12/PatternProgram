e46f2f298c1ad15bae8590fe47236be332f41d05
yadavpriya12 <169046727+yadavpriya12@users.noreply.github.com>
2024-08-19 19:41:33 +0530

    Add files via upload

---
+++ _21Pattern.java
@@ -0,0 +1,12 @@
+public class _21Pattern {
+    public static void main(String[] args) {
+        for(int i=7;i>=1;i--)
+        {
+        for(int j=1;j<=i;j++)
+        {
+            System.out.print(j%2);
+        }
+        System.out.println(" ");
+    }
+    }
+}
