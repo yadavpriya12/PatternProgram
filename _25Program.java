7706c25ec19a419dfccc51da5a1738ac896b79f0
yadavpriya12 <169046727+yadavpriya12@users.noreply.github.com>
2024-08-28 06:09:39 +0530

    Add files via upload

---
+++ _25Program.java
@@ -0,0 +1,17 @@
+public class _25Program {
+    public static void main(String[] args) {
+        for(int i=1;i<=11;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.print(" ");
+            }
+
+            for(int j=1;j<=11;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
