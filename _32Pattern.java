d352dda0c3d968f378a2e008b3ac44fdf3aaebca
yadavpriya12 <169046727+yadavpriya12@users.noreply.github.com>
2024-09-02 12:11:15 +0530

    Add files via upload

---
+++ _32Pattern.java
@@ -0,0 +1,16 @@
+public class _32Pattern {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++)
+        {
+            for(int k=1;k<=i-1;k++)
+            {
+            System.out.print(" ");
+            }
+            for(int j=1;j<=5;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
